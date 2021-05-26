40256955ff27397b2c947d663a66275ed119998b
PastLeo <chgu82837@gmail.com>
2021-05-26 16:26:55 +0800

    JSON.generate() works in browser

--- src/Ruby.worker.js
+++ src/Ruby.worker.js
@@ -40,16 +40,56 @@ const Module = {
   }
 };
 
+const GEMS = {
+  // how to generate file list for a gem:
+  // $ cd public/gems/xxx/lib
+  // $ ruby -e 'pp Dir["**/*.rb"]'
+  ostruct: ["ostruct.rb"],
+  json: [
+    "json.rb", "json/common.rb", "json/version.rb", "json/generic_object.rb",
+    "json/pure/generator.rb", "json/pure/parser.rb",
+    "json/pure.rb",
+  ],
+  kamiflex: [
+    "kamiflex.rb", "kamiflex/railtie.rb", "kamiflex/actions.rb",
+    "kamiflex/core.rb", "kamiflex/basic_elements.rb",
+    "kamiflex/version.rb",
+    "kamiflex/quick_reply.rb",
+    "kamiflex/custom.rb",
+  ],
+}
+
 // Initialize the module
 run(Module).then(function() {
   // Module is ready to run
   // Wait until we have input
-  input.then(function(input) {
+  input.then(async function(input) {
     // Write the input file
-    Module.FS.writeFile("playground.rb", input);
+    Module.FS.writeFile(
+      "playground.rb", `
+        ${Object.keys(GEMS).map(gem => `$LOAD_PATH.push('/gems/${gem}/lib');`).join('\n')}
+        require 'json';
+        ${input}
+      `
+    );
+
+    await Promise.all(
+      Object.entries(GEMS)
+        .flatMap(([gem, rubies]) => rubies.map(rb => `/gems/${gem}/lib/${rb}`))
+        .map(async f => {
+          const rbScript = await (await fetch(f)).text();
+          //console.log(rbScript)
+          Module.FS.mkdirTree(f.split('/').slice(0, -1).join('/'))
+          Module.FS.writeFile(f, rbScript);
+        })
+    );
+
+    console.log(Module['buffer'])
+    //console.log(JSON.stringify(Object.keys(Module.FS)))
 
     // Run main()
     Module.callMain(["playground.rb"]);
+    //Module.callMain(["--help"]);
 
     // Indicate completion
     self.postMessage({complete: true});
